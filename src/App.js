d2a693558f7c5f872ba509ef05e7be4c41fab332
Daria <daria.zhuravlova23@gmail.com>
2022-01-03 21:03:42 +0200

    hw3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import * as React from 'react';
+import SignupForm from './Components/SignupForm';
+import LoginForm from './Components/LoginForm';
+import Tabs from '@mui/material/Tabs';
+import Typography from '@mui/material/Typography';
+import TabPanel from '@mui/lab/TabPanel';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid'
 import './App.css';
 
+
 function App() {
+    const [value, setValue] = React.useState('1');
+  
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container justifyContent="center">
+    <Box sx={{ width: '25%', typography: 'body1' }}>
+    <TabContext value={value}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <TabList onChange={handleChange} aria-label="lab API tabs example">
+          <Tab label="LOG IN" value="1" />
+          <Tab label="SIGN UP" value="2" />
+        </TabList>
+      </Box>
+      <TabPanel value="1"><LoginForm/></TabPanel>
+      <TabPanel value="2"><SignupForm/></TabPanel>
+    </TabContext>
+  </Box>
+  </Grid>
   );
 }
-
 export default App;
---
+++ src/Components/SignupForm.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+const validationSchema = yup.object({
+  email: yup
+    .string('Enter your email')
+    .email('Enter a valid email')
+    .required('Email is required'),
+  password: yup
+    .string('Enter your password')
+    .min(8, 'Password should be of minimum 8 characters length')
+    .required('Password is required'),
+  confirm: yup
+  .string().when("password", {
+    is: val => (val && val.length > 0 ? true : false),
+    then: yup.string().oneOf(
+      [yup.ref("password")],
+      "Both password need to be the same"
+    )
+    })
+  });
+
+
+const SignupForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      confirm: ''
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  return (
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField
+          fullWidth
+          id="email"
+          name="email"
+          label="Email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+        />
+        <TextField
+          fullWidth
+          id="password"
+          name="password"
+          label="Password"
+          type="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          error={formik.touched.password && Boolean(formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
+        />
+        <TextField
+          fullWidth
+          id="confirm"
+          name="confirm"
+          label="Confirm Password"
+          type="password"
+          value={formik.values.confirm}
+          onChange={formik.handleChange}
+          error={formik.touched.confirm && Boolean(formik.errors.confirm)}
+          helperText={formik.touched.confirm && formik.errors.confirm}
+        />
+        <Button color="primary" variant="contained" fullWidth type="submit">
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default SignupForm;
